fc1c8478245d167af2dd90807e41663356f74960
zhijia,.zhang <jiahut@gmail.com>
2014-08-04 10:44:11 +0800

    add the test_in case

--- filter.test.py
+++ filter.test.py
@@ -4,19 +4,26 @@ import subprocess
 import os
 
 class Filter(unittest.TestCase):
+
     def test_like(self):
         # result = commands.getoutput("cat /tmp/_filter.test.log | filter like '.3.5' ")
         like_process = subprocess.Popen("cat /tmp/_filter.test.log | filter like '.3.5'", stdout=subprocess.PIPE, shell=True)
         output,_ = like_process.communicate()
         self.assertEqual(output.strip(),"2345")
 
+    def test_in(self):
+        in_process = subprocess.Popen("cat /tmp/_filter.test.log | filter in /tmp/_filter.test.log", stdout=subprocess.PIPE, shell=True)
+        output,_ = in_process.communicate()
+        self.assertEqual(output.strip(),self.text)
+
     def setUp(self):
-        with open("/tmp/_filter.test.log","w") as file:
-            file.write('''\
+        self.text = '''\
 12345
 2345
 345\
-''')
+'''
+        with open("/tmp/_filter.test.log","w") as file:
+            file.write(self.text)
 
 
     def tearDown(self):
